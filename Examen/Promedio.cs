963533e506a39c65bed6d4ef062cb81f679923e0
JasonN13 <148019428+JasonN13@users.noreply.github.com>
2023-11-08 19:31:42 -0600

    Update Promedio.cs
    Actualizacion2

--- Examen/Promedio.cs
+++ Examen/Promedio.cs
@@ -8,5 +8,20 @@ namespace Examen
 {
     internal class Promedio
     {
+        public double nota1;
+        public double nota2;
+        public double nota3;
+
+
+       public Promedio(double nota1, double nota2,double nota3)
+        {
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+            this.nota3 = nota3;
+        }
+        public string mostrarpromedio()
+        {
+
+        }
     }
 }
